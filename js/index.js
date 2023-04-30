8bb0d34785450e236ef159d6debb376f309a6f92
apamanger <rfoley00@live.com>
2023-04-29 22:08:32 -0400

    updating info adding javscript functions

---
+++ js/index.js
@@ -0,0 +1,13 @@
+
+
+
+
+function img_increase()
+        {
+            document.getElementById("image").style.width="250px";
+        } 
+
+function img_decrease()
+        {
+           document.getElementById("image").style.width="100px";
+        }
